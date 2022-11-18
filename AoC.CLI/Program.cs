75d3f07d890e96e5fafd8505db7884825daa0f31
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2022-11-18 20:04:04 +0000

    Added list command to CLI

--- AoC.CLI/Program.cs
+++ AoC.CLI/Program.cs
@@ -11,27 +11,41 @@ static void PrintTitle()
 
 PrintTitle();
 
-bool exit;
+var exit = false;
 var defaultDay = SolverFactory.Instance.DefaultDay;
 var cliDays = new Queue<string>(args.Length > 0 ? args : new[] { "" });
 do
 {
-    Console.WriteLine(Green($"Type day number or blank for {defaultDay} or 'x' to exit"));
+    Console.WriteLine(Green($"Type day number, or blank for {defaultDay}, or 'list', or 'x' to exit"));
     var dayNumber = cliDays.TryDequeue(out var cliDay) ? cliDay : Console.ReadLine() ?? "x";
     dayNumber = string.IsNullOrWhiteSpace(dayNumber) ? defaultDay : dayNumber;
 
-    exit = dayNumber is "x" or "exit";
-    if (!exit)
+    switch (dayNumber)
     {
-        PrintTitle();
-        var solver = SolverFactory.Instance.TryCreateSolver(dayNumber);
-        if (solver != null)
-        {
-            await solver.RunAsync();
-        }
-        else
-        {
-            Console.WriteLine(Red($"No solver for day '{Bright.Cyan(dayNumber)}'."));
-        }
+        case "x":
+        case "exit":
+            exit = true;
+            break;
+
+        case "list":
+            PrintTitle();
+            Console.WriteLine(string.Join(
+                Environment.NewLine,
+                SolverFactory.Instance.Solvers.Where(x => !string.IsNullOrEmpty(x.DayName)).Select(x => $"{x.DayNumber}: {x.DayName}")));
+            break;
+
+        default:
+            PrintTitle();
+            var solver = SolverFactory.Instance.TryCreateSolver(dayNumber);
+            if (solver != null)
+            {
+                await solver.RunAsync();
+            }
+            else
+            {
+                Console.WriteLine(Red($"No solver for day '{Bright.Cyan(dayNumber)}'."));
+            }
+
+            break;
     }
 } while (!exit);
