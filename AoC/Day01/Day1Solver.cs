0f930cdc039247e02c57e405f40b867838f27265
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2022-12-01 09:45:08 +0000

    Day 1 minor code tidy

--- AoC/Day01/Day1Solver.cs
+++ AoC/Day01/Day1Solver.cs
@@ -6,13 +6,12 @@ public class Day1Solver : SolverBase
 {
     public override string DayName => "Calorie Counting";
 
-    private static IEnumerable<long> ParseElves(PuzzleInput input) =>
-        input.ToString()
-            .Split(NewLine + NewLine)
-            .Select(block => block.Split(NewLine).Select(long.Parse))
+    private static IEnumerable<long> GetEachElfTotalCalories(PuzzleInput input) =>
+        input.ToString().Split(NewLine + NewLine)
+            .Select(elfInventory => elfInventory.Split(NewLine).Select(long.Parse))
             .Select(elfCalories => elfCalories.Sum());
 
-    public override long? SolvePart1(PuzzleInput input) => ParseElves(input).Max(totalCalories => totalCalories);
+    public override long? SolvePart1(PuzzleInput input) => GetEachElfTotalCalories(input).Max();
 
-    public override long? SolvePart2(PuzzleInput input) => ParseElves(input).OrderByDescending(totalCalories => totalCalories).Take(3).Sum();
+    public override long? SolvePart2(PuzzleInput input) => GetEachElfTotalCalories(input).OrderDescending().Take(3).Sum();
 }
