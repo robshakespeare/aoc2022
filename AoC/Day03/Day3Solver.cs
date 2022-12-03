6b3760c870af918961c5c492b700969d593f96ac
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2022-12-03 05:49:59 +0000

    Day 3 tidy

--- AoC/Day03/Day3Solver.cs
+++ AoC/Day03/Day3Solver.cs
@@ -1,50 +1,39 @@
+using System.Collections.Concurrent;
+
 namespace AoC.Day03;
 
 public class Day3Solver : SolverBase
 {
-    public override string DayName => "";
+    public override string DayName => "Rucksack Reorganization";
 
-    private static int GetPriority(char c) => char.IsLower(c) ? 1 + c - 'a' : 27 + c - 'A';
+    static int GetPriority(char c) => char.IsLower(c) ? 1 + c - 'a' : 27 + c - 'A';
 
-    public override long? SolvePart1(PuzzleInput input)
+    static char GetCommonCharacter(IReadOnlyCollection<string> group)
     {
-        return input.ReadLines()
-            .Select(line => new
-            {
-                compartment1 = line[..(line.Length / 2)],
-                compartment2 = line[(line.Length / 2)..]
-            })
-            .Select(compartments => compartments.compartment1.Join(compartments.compartment2, c => c, c => c, (c, _) => c).First())
-            .Sum(GetPriority);
-    }
+        var sacksByChar = new ConcurrentDictionary<char, List<string>>();
 
-    public override long? SolvePart2(PuzzleInput input)
-    {
-        char GetGroupBadge(IEnumerable<string> group)
+        foreach (var sack in group)
         {
-            var dict = new Dictionary<char, List<string>>();
-
-            foreach (var sack in group)
+            foreach (var c in sack.Distinct())
             {
-                foreach (var c in sack.Distinct())
-                {
-                    if (!dict.ContainsKey(c))
-                    {
-                        dict[c] = new List<string>();
-                    }
-
-                    dict[c].Add(sack);
-                }
+                sacksByChar.GetOrAdd(c, _ => new List<string>()).Add(sack);
             }
-
-            return dict.Single(x => x.Value.Count == 3).Key;
         }
 
-        return input.ReadLines()
+        return sacksByChar.Single(x => x.Value.Count == group.Count).Key;
+    }
+
+    public override long? SolvePart1(PuzzleInput input) =>
+        input.ReadLines()
+            .Select(line => new[] { line[..(line.Length / 2)], line[(line.Length / 2)..] })
+            .Select(GetCommonCharacter)
+            .Sum(GetPriority);
+
+    public override long? SolvePart2(PuzzleInput input) =>
+        input.ReadLines()
             .Select((line, i) => (line, i))
             .GroupBy(x => x.i / 3)
-            .Select(g => g.Select(sack => sack.line))
-            .Select(GetGroupBadge)
+            .Select(g => g.Select(sack => sack.line).ToArray())
+            .Select(GetCommonCharacter)
             .Sum(GetPriority);
-    }
 }
