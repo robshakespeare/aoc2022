37d3c87e6c77c30080b418e9121903ea7bc275b5
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2022-12-03 06:12:19 +0000

    Day 3 more tidy

--- AoC/Day03/Day3Solver.cs
+++ AoC/Day03/Day3Solver.cs
@@ -1,5 +1,3 @@
-using System.Collections.Concurrent;
-
 namespace AoC.Day03;
 
 public class Day3Solver : SolverBase
@@ -8,32 +6,23 @@ public class Day3Solver : SolverBase
 
     static int GetPriority(char c) => char.IsLower(c) ? 1 + c - 'a' : 27 + c - 'A';
 
-    static char GetCommonCharacter(IReadOnlyCollection<string> group)
-    {
-        var sacksByChar = new ConcurrentDictionary<char, List<string>>();
-
-        foreach (var sack in group)
-        {
-            foreach (var c in sack.Distinct())
-            {
-                sacksByChar.GetOrAdd(c, _ => new List<string>()).Add(sack);
-            }
-        }
-
-        return sacksByChar.Single(x => x.Value.Count == group.Count).Key;
-    }
+    static char GetCommonCharacter(IReadOnlyCollection<string> group) => group
+        .SelectMany(sack => sack.Select(c => (c, sack)))
+        .GroupBy(x => x.c)
+        .First(g => g.Select(x => x.sack).Distinct().Count() == group.Count)
+        .Key;
 
     public override long? SolvePart1(PuzzleInput input) =>
         input.ReadLines()
-            .Select(line => new[] { line[..(line.Length / 2)], line[(line.Length / 2)..] })
+            .Select(line => new[] {line[..(line.Length / 2)], line[(line.Length / 2)..]})
             .Select(GetCommonCharacter)
             .Sum(GetPriority);
 
     public override long? SolvePart2(PuzzleInput input) =>
         input.ReadLines()
-            .Select((line, i) => (line, i))
-            .GroupBy(x => x.i / 3)
-            .Select(g => g.Select(sack => sack.line).ToArray())
+            .Select((sack, idx) => (sack, idx))
+            .GroupBy(x => x.idx / 3)
+            .Select(g => g.Select(x => x.sack).ToArray())
             .Select(GetCommonCharacter)
             .Sum(GetPriority);
 }
