fde1771dd304dbe629c8bead6efdbb38f10031cf
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2022-12-06 05:37:40 +0000

    Day 6 tidied

--- AoC/Day06/Day6Solver.cs
+++ AoC/Day06/Day6Solver.cs
@@ -4,23 +4,11 @@ public class Day6Solver : SolverBase
 {
     public override string DayName => "Tuning Trouble";
 
-    public override long? SolvePart1(PuzzleInput input) => FindMarkerInDataStream(input, 4, "start-of-packet");
+    public override long? SolvePart1(PuzzleInput input) => FindMarkerInDataStream(input, 4);
 
-    public override long? SolvePart2(PuzzleInput input) => FindMarkerInDataStream(input, 14, "start-of-message");
+    public override long? SolvePart2(PuzzleInput input) => FindMarkerInDataStream(input, 14);
 
-    static int FindMarkerInDataStream(string dataStream, int markerLength, string markerName)
-    {
-        for (var i = 0; i < dataStream.Length; i++)
-        {
-            var candidatePosition = i + markerLength;
-            var candidate = dataStream[i..candidatePosition];
-
-            if (candidate.Distinct().Count() == markerLength)
-            {
-                return candidatePosition;
-            }
-        }
-
-        throw new InvalidOperationException($"Failed to find {markerName} marker");
-    }
+    static int FindMarkerInDataStream(string dataStream, int markerLength) => Enumerable
+        .Range(markerLength, dataStream.Length - markerLength)
+        .First(candidatePosition => dataStream[(candidatePosition - markerLength)..candidatePosition].Distinct().Count() == markerLength);
 }
