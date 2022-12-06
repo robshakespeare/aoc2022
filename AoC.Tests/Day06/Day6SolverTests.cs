c621ef4c55094d0b66e2eb9eb3566269bde7f60f
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2022-12-06 05:31:48 +0000

    Day 6 tidying

--- AoC.Tests/Day06/Day6SolverTests.cs
+++ AoC.Tests/Day06/Day6SolverTests.cs
@@ -12,14 +12,18 @@ public class Day6SolverTests
     private const string ExampleInput4 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
     private const string ExampleInput5 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
 
-    [Test]
-    public void Part1Examples()
+    [TestCase(ExampleInput1, 7)]
+    [TestCase(ExampleInput2, 5)]
+    [TestCase(ExampleInput3, 6)]
+    [TestCase(ExampleInput4, 10)]
+    [TestCase(ExampleInput5, 11)]
+    public void Part1Examples(string exampleInput, int expectedResult)
     {
         // ACT
-        var part1ExampleResult = _sut.SolvePart1(ExampleInput);
+        var part1ExampleResult = _sut.SolvePart1(exampleInput);
 
         // ASSERT
-        part1ExampleResult.Should().Be(7);
+        part1ExampleResult.Should().Be(expectedResult);
     }
 
     [Test]
@@ -32,14 +36,18 @@ public class Day6SolverTests
         part1Result.Should().Be(1794);
     }
 
-    [Test]
-    public void Part2Example()
+    [TestCase(ExampleInput1, 19)]
+    [TestCase(ExampleInput2, 23)]
+    [TestCase(ExampleInput3, 23)]
+    [TestCase(ExampleInput4, 29)]
+    [TestCase(ExampleInput5, 26)]
+    public void Part2Examples(string exampleInput, int expectedResult)
     {
         // ACT
-        var part2ExampleResult = _sut.SolvePart2(ExampleInput);
+        var part2ExampleResult = _sut.SolvePart2(exampleInput);
 
         // ASSERT
-        part2ExampleResult.Should().Be(19);
+        part2ExampleResult.Should().Be(expectedResult);
     }
 
     [Test]
--- AoC/Day06/Day6Solver.cs
+++ AoC/Day06/Day6Solver.cs
@@ -2,41 +2,25 @@ namespace AoC.Day06;
 
 public class Day6Solver : SolverBase
 {
-    public override string DayName => "";
+    public override string DayName => "Tuning Trouble";
 
-    public override long? SolvePart1(PuzzleInput input)
+    public override long? SolvePart1(PuzzleInput input) => FindMarkerInDataStream(input, 4, "start-of-packet");
+
+    public override long? SolvePart2(PuzzleInput input) => FindMarkerInDataStream(input, 14, "start-of-message");
+
+    static int FindMarkerInDataStream(string dataStream, int markerLength, string markerName)
     {
-        var str = input.ToString();
-
-        for (var i = 3; i < str.Length; i++)
+        for (var i = 0; i < dataStream.Length; i++)
         {
-            var candidatePosition = i + 4;
-            var candidate = str[i..candidatePosition];
+            var candidatePosition = i + markerLength;
+            var candidate = dataStream[i..candidatePosition];
 
-            if (candidate.Distinct().Count() == 4)
+            if (candidate.Distinct().Count() == markerLength)
             {
                 return candidatePosition;
             }
         }
 
-        throw new InvalidOperationException("Failed to find start-of-packet marker");
-    }
-
-    public override long? SolvePart2(PuzzleInput input)
-    {
-        var str = input.ToString();
-
-        for (var i = 3; i < str.Length; i++)
-        {
-            var candidatePosition = i + 14;
-            var candidate = str[i..candidatePosition];
-
-            if (candidate.Distinct().Count() == 14)
-            {
-                return candidatePosition;
-            }
-        }
-
-        throw new InvalidOperationException("Failed to find start-of-message marker");
+        throw new InvalidOperationException($"Failed to find {markerName} marker");
     }
 }
