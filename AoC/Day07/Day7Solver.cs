c68c4d4c12ad0785e63396edc04859e8897fa3b7
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2022-12-07 07:07:19 +0000

    Changed Day 7 to use queue to avoid initial reverse operation; and minor shortening of "subDirectory" to "subDir"; and standardize no braces for switch cases; and improved name of 'ListAllDirectories' method; and reduced code in 'ParseFilesystem' method.

--- AoC/Day07/Day7Solver.cs
+++ AoC/Day07/Day7Solver.cs
@@ -8,7 +8,7 @@ public class Day7Solver : ISolver
     {
         var rootDir = ParseFilesystem(input);
 
-        return rootDir.ListAll().Where(dir => dir.TotalSize <= 100000).Sum(dir => dir.TotalSize);
+        return rootDir.ListAllDirectories().Where(dir => dir.TotalSize <= 100000).Sum(dir => dir.TotalSize);
     }
 
     public long? SolvePart2(PuzzleInput input)
@@ -22,7 +22,7 @@ public class Day7Solver : ISolver
         var totalAmountOfUnusedSpace = totalDiskSpaceAvailable - totalAmountOfUsedSpace;
         var minRequiredSizeToDelete = minRequiredUnusedSpace - totalAmountOfUnusedSpace;
 
-        return rootDir.ListAll()
+        return rootDir.ListAllDirectories()
             .Where(x => x.TotalSize >= minRequiredSizeToDelete)
             .MinBy(x => x.TotalSize)
             ?.TotalSize;
@@ -50,20 +50,20 @@ public class Day7Solver : ISolver
 
         public static ElfDir NewRoot() => new("/", null);
 
-        public ElfDir GetDirectory(string subDirectoryName) =>
-            _subDirectories.TryGetValue(subDirectoryName, out var subDirectory)
-                ? subDirectory
-                : throw new InvalidOperationException($"No sub directory called '{subDirectoryName}' in '{Path}'");
+        public ElfDir GetDirectory(string subDirName) =>
+            _subDirectories.TryGetValue(subDirName, out var subDir)
+                ? subDir
+                : throw new InvalidOperationException($"No sub directory called '{subDirName}' in '{Path}'");
 
-        public void AddSubDirectory(string subDirectoryName) => _subDirectories.Add(subDirectoryName, new ElfDir($"{Path}{subDirectoryName}/", this));
+        public void AddSubDirectory(string subDirName) => _subDirectories.Add(subDirName, new ElfDir($"{Path}{subDirName}/", this));
 
         public void AddFile(string fileName, long fileSize) => _files.Add(fileName, fileSize);
 
-        public IEnumerable<ElfDir> ListAll()
+        public IEnumerable<ElfDir> ListAllDirectories()
         {
             yield return this;
 
-            foreach (var subSubDir in _subDirectories.Values.SelectMany(subDir => subDir.ListAll()))
+            foreach (var subSubDir in _subDirectories.Values.SelectMany(subDir => subDir.ListAllDirectories()))
             {
                 yield return subSubDir;
             }
@@ -74,13 +74,12 @@ public class Day7Solver : ISolver
 
     static ElfDir ParseFilesystem(string input)
     {
-        var rootDirectory = ElfDir.NewRoot();
-        var currentDirectory = rootDirectory;
-        var inputLines = new Stack<string>(input.ReadLines().Reverse());
+        var currentDirectory = ElfDir.NewRoot();
+        var inputLines = new Queue<string>(input.ReadLines());
 
         while (inputLines.Count > 0)
         {
-            var line = inputLines.Pop();
+            var line = inputLines.Dequeue();
             var parts = line.Split(" ");
 
             if (parts[0] == "$")
@@ -90,10 +89,8 @@ public class Day7Solver : ISolver
                 switch (command)
                 {
                     case "cd":
-                    {
                         currentDirectory = ChangeDirectoryCommand(parts[2], currentDirectory);
                         break;
-                    }
                     case "ls":
                         ListCommand(inputLines, currentDirectory);
                         break;
@@ -107,7 +104,7 @@ public class Day7Solver : ISolver
             }
         }
 
-        return rootDirectory;
+        return currentDirectory.Root;
     }
 
     private static ElfDir ChangeDirectoryCommand(string arg, ElfDir currentDirectory)
@@ -129,11 +126,11 @@ public class Day7Solver : ISolver
         }
     }
 
-    static void ListCommand(Stack<string> inputLines, ElfDir currentDirectory)
+    static void ListCommand(Queue<string> inputLines, ElfDir currentDirectory)
     {
         while (inputLines.Count > 0 && !inputLines.Peek().StartsWith("$"))
         {
-            var contents = inputLines.Pop().Split(" ");
+            var contents = inputLines.Dequeue().Split(" ");
 
             if (contents[0] == "dir")
             {
