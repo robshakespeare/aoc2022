a3a3313eba449ed8087a449cceee3aebba8b6e8b
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2022-12-10 05:26:37 +0000

    Day 10 part 1 done

--- AoC.Tests/Day10/Day10SolverTests.cs
+++ AoC.Tests/Day10/Day10SolverTests.cs
@@ -6,8 +6,6 @@ public class Day10SolverTests
 {
     private readonly Day10Solver _sut = new();
 
-    private const string ExampleInput = @"";
-
     [Test]
     public void Part1Example()
     {
@@ -15,7 +13,7 @@ public class Day10SolverTests
         var part1ExampleResult = _sut.SolvePart1(ExampleInput);
 
         // ASSERT
-        part1ExampleResult.Should().Be(null);
+        part1ExampleResult.Should().Be(13140);
     }
 
     [Test]
@@ -25,7 +23,7 @@ public class Day10SolverTests
         var part1Result = _sut.SolvePart1();
 
         // ASSERT
-        part1Result.Should().Be(null);
+        part1Result.Should().Be(13720);
     }
 
     [Test]
@@ -47,4 +45,153 @@ public class Day10SolverTests
         // ASSERT
         part2Result.Should().Be(null);
     }
+
+    private const string ExampleInput = """
+        addx 15
+        addx -11
+        addx 6
+        addx -3
+        addx 5
+        addx -1
+        addx -8
+        addx 13
+        addx 4
+        noop
+        addx -1
+        addx 5
+        addx -1
+        addx 5
+        addx -1
+        addx 5
+        addx -1
+        addx 5
+        addx -1
+        addx -35
+        addx 1
+        addx 24
+        addx -19
+        addx 1
+        addx 16
+        addx -11
+        noop
+        noop
+        addx 21
+        addx -15
+        noop
+        noop
+        addx -3
+        addx 9
+        addx 1
+        addx -3
+        addx 8
+        addx 1
+        addx 5
+        noop
+        noop
+        noop
+        noop
+        noop
+        addx -36
+        noop
+        addx 1
+        addx 7
+        noop
+        noop
+        noop
+        addx 2
+        addx 6
+        noop
+        noop
+        noop
+        noop
+        noop
+        addx 1
+        noop
+        noop
+        addx 7
+        addx 1
+        noop
+        addx -13
+        addx 13
+        addx 7
+        noop
+        addx 1
+        addx -33
+        noop
+        noop
+        noop
+        addx 2
+        noop
+        noop
+        noop
+        addx 8
+        noop
+        addx -1
+        addx 2
+        addx 1
+        noop
+        addx 17
+        addx -9
+        addx 1
+        addx 1
+        addx -3
+        addx 11
+        noop
+        noop
+        addx 1
+        noop
+        addx 1
+        noop
+        noop
+        addx -13
+        addx -19
+        addx 1
+        addx 3
+        addx 26
+        addx -30
+        addx 12
+        addx -1
+        addx 3
+        addx 1
+        noop
+        noop
+        noop
+        addx -9
+        addx 18
+        addx 1
+        addx 2
+        noop
+        noop
+        addx 9
+        noop
+        noop
+        noop
+        addx -1
+        addx 2
+        addx -37
+        addx 1
+        addx 3
+        noop
+        addx 15
+        addx -21
+        addx 22
+        addx -6
+        addx 1
+        noop
+        addx 2
+        addx 1
+        noop
+        addx -10
+        noop
+        noop
+        addx 20
+        addx 1
+        addx 2
+        addx 2
+        addx -6
+        addx -11
+        noop
+        noop
+        noop
+        """;
 }
--- AoC/Day10/Day10Solver.cs
+++ AoC/Day10/Day10Solver.cs
@@ -2,15 +2,52 @@ namespace AoC.Day10;
 
 public class Day10Solver : ISolver
 {
-    public string DayName => "";
+    public string DayName => "Cathode-Ray Tube";
 
     public long? SolvePart1(PuzzleInput input)
     {
-        return null;
+        foreach (var output in ParseAndProcessInstructions(input))
+        {
+            Console.WriteLine(output);
+        }
+
+        return ParseAndProcessInstructions(input).Aggregate(0L, (agg, cur) => agg + cur.CycleNumber * cur.RegisterX);
     }
 
     public long? SolvePart2(PuzzleInput input)
     {
         return null;
     }
+
+    static IEnumerable<(long CycleNumber, long RegisterX)> ParseAndProcessInstructions(PuzzleInput input)
+    {
+        var registerX = 1;
+        var cycleNumber = 0;
+        foreach (var instruction in ParseInstructions(input))
+        {
+            for (var i = 0; i < instruction.CycleLength; i++)
+            {
+                cycleNumber++;
+
+                if ((cycleNumber + 20) % 40 == 0)
+                {
+                    yield return (cycleNumber, registerX);
+                }
+            }
+
+            registerX += instruction.RegisterXDelta;
+        }
+    }
+
+    public record Instruction(string Command, int RegisterXDelta, int CycleLength);
+
+    static IEnumerable<Instruction> ParseInstructions(PuzzleInput input) =>
+        input.ReadLines()
+            .Select(line => line.Split(" "))
+            .Select(parts => parts switch
+            {
+                ["addx", var amount] => new Instruction("addx", int.Parse(amount), 2),
+                ["noop"] => new Instruction("noop", 0, 1),
+                _ => throw new InvalidOperationException("Invalid command: " + parts[0])
+            });
 }
