b78c2139534bf29f4e90d790471b32217539de41
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2022-12-15 05:52:47 +0000

    Day 15 part 1 done, needs tidy!

--- AoC.Tests/Day15/Day15SolverTests.cs
+++ AoC.Tests/Day15/Day15SolverTests.cs
@@ -6,7 +6,22 @@ public class Day15SolverTests
 {
     private readonly Day15Solver _sut = new();
 
-    private const string ExampleInput = @"";
+    private const string ExampleInput = """
+        Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+        Sensor at x=9, y=16: closest beacon is at x=10, y=16
+        Sensor at x=13, y=2: closest beacon is at x=15, y=3
+        Sensor at x=12, y=14: closest beacon is at x=10, y=16
+        Sensor at x=10, y=20: closest beacon is at x=10, y=16
+        Sensor at x=14, y=17: closest beacon is at x=10, y=16
+        Sensor at x=8, y=7: closest beacon is at x=2, y=10
+        Sensor at x=2, y=0: closest beacon is at x=2, y=10
+        Sensor at x=0, y=11: closest beacon is at x=2, y=10
+        Sensor at x=20, y=14: closest beacon is at x=25, y=17
+        Sensor at x=17, y=20: closest beacon is at x=21, y=22
+        Sensor at x=16, y=7: closest beacon is at x=15, y=3
+        Sensor at x=14, y=3: closest beacon is at x=15, y=3
+        Sensor at x=20, y=1: closest beacon is at x=15, y=3
+        """;
 
     [Test]
     public void Part1Example()
@@ -15,7 +30,7 @@ public class Day15SolverTests
         var part1ExampleResult = _sut.SolvePart1(ExampleInput);
 
         // ASSERT
-        part1ExampleResult.Should().Be(null);
+        part1ExampleResult.Should().Be(26);
     }
 
     [Test]
@@ -25,7 +40,7 @@ public class Day15SolverTests
         var part1Result = _sut.SolvePart1();
 
         // ASSERT
-        part1Result.Should().Be(null);
+        part1Result.Should().Be(5127797);
     }
 
     [Test]
--- AoC/Day15/Day15Solver.cs
+++ AoC/Day15/Day15Solver.cs
@@ -2,15 +2,114 @@ namespace AoC.Day15;
 
 public class Day15Solver : ISolver
 {
-    public string DayName => "";
+    public string DayName => "Beacon Exclusion Zone";
+
+    const char SensorChar = 'S';
+    const char BeaconChar = 'B';
+    const char NoBeaconChar = '#';
 
     public long? SolvePart1(PuzzleInput input)
     {
-        return null;
+        var sensorReports = ParseSensorReports(input);
+        var targetY = sensorReports.Length < 30 ? 10 : 2000000;
+
+        var map = new Dictionary<Vector2, char>();
+
+        foreach (var sensorReport in sensorReports)
+        {
+            //if (map.ContainsKey(sensorReport.SensorPosition))
+            //{
+            //    throw new InvalidOperationException($"Sensor at {sensorReport.SensorPosition} would overlap {map[sensorReport.SensorPosition]} which is already there");
+            //}
+
+            //if (map.ContainsKey(sensorReport.BeaconPosition))
+            //{
+            //    throw new InvalidOperationException($"Beacon at {sensorReport.BeaconPosition} would overlap {map[sensorReport.BeaconPosition]} which is already there");
+            //}
+
+            //map.Add(sensorReport.SensorPosition, SensorChar);
+            //map.Add(sensorReport.BeaconPosition, BeaconChar);
+
+            map[sensorReport.SensorPosition] = SensorChar;
+            map[sensorReport.BeaconPosition] = BeaconChar;
+
+            PlotSensorReportCoverage(sensorReport, map, targetY);
+        }
+
+        return map.Count(p => (int) p.Key.Y == targetY && p.Value == NoBeaconChar);
+    }
+
+    static void PlotSensorReportCoverage(SensorReport sensorReport, Dictionary<Vector2, char> map, int targetY)
+    {
+        var width = sensorReport.DistanceBetweenSensorAndBeacon * 2 + 1;
+
+        var sensorX = (int) sensorReport.SensorPosition.X;
+        var sensorY = (int) sensorReport.SensorPosition.Y;
+        var relativeY = 0;
+
+        while (width > 0)
+        {
+            var northY = sensorY - relativeY;
+            var southY = sensorY + relativeY;
+
+            if (northY == targetY)
+            {
+                PlotPath(
+                    new Vector2(sensorX - (int)(width / 2f), northY),
+                    new Vector2(sensorX + (int)(width / 2f), northY),
+                    map);
+            }
+
+            if (southY == targetY)
+            {
+                PlotPath(
+                    new Vector2(sensorX - (int)(width / 2f), southY),
+                    new Vector2(sensorX + (int)(width / 2f), southY),
+                    map);
+            }
+
+            // new Vector2(SandSource.X - (int) (floorWidth / 2f), floorY),
+            // new Vector2(SandSource.X + (int)(floorWidth / 2f), floorY)
+
+            width -= 2;
+            relativeY++;
+        }
+    }
+
+    static void PlotPath(Vector2 left, Vector2 right, Dictionary<Vector2, char> map)
+    {
+        if (right.X < left.X)
+        {
+            throw new InvalidOperationException("Invalid state");
+        }
+
+        var current = left;
+        var dir = new Vector2(1, 0); // : Vector2.Normalize(right - left);
+        var target = right + dir;
+        while (current != target)
+        {
+            map.TryAdd(current, NoBeaconChar);
+            current += dir;
+        }
     }
 
     public long? SolvePart2(PuzzleInput input)
     {
         return null;
     }
+
+    public record SensorReport(Vector2 SensorPosition, Vector2 BeaconPosition)
+    {
+        public long DistanceBetweenSensorAndBeacon { get; } = MathUtils.ManhattanDistance(BeaconPosition, SensorPosition);
+    }
+
+    static SensorReport[] ParseSensorReports(string input) =>
+        ParseLineRegex.Matches(input).Select(match => new SensorReport(
+            new Vector2(int.Parse(match.Groups["sX"].Value), int.Parse(match.Groups["sY"].Value)),
+            new Vector2(int.Parse(match.Groups["bX"].Value), int.Parse(match.Groups["bY"].Value)))).ToArray();
+
+    // Sensor at x=(?<sX>-?\d+), y=(?<sY>-?\d+): closest beacon is at x=(?<bX>-?\d+), y=(?<bY>-?\d+)
+    private static readonly Regex ParseLineRegex = new(
+        @"Sensor at x=(?<sX>-?\d+), y=(?<sY>-?\d+): closest beacon is at x=(?<bX>-?\d+), y=(?<bY>-?\d+)",
+        RegexOptions.Compiled);
 }
