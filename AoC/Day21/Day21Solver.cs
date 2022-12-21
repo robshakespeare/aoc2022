7c98ece311af262272daab4d797604fd190f5268
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2022-12-21 07:44:09 +0000

    Day 21 tidied

--- AoC/Day21/Day21Solver.cs
+++ AoC/Day21/Day21Solver.cs
@@ -1,5 +1,3 @@
-using Crayon;
-
 namespace AoC.Day21;
 
 public class Day21Solver : ISolver
@@ -21,46 +19,19 @@ public class Day21Solver : ISolver
 
         var rootMonkey = (MathMonkey) monkeys["root"];
 
-        //foreach (var yell in new[] { -10000, 0, 1, 301, 1000, 10000, 1_000_000, 1_000_000_000, 1_000_000_000_000, 2_000_000_000_000, 5_000_000_000_000, long.MaxValue })
-        foreach (var yell in new[] { 0, 1, 100, 301, 1000, 10000, 1_000_000_000_000, 2_000_000_000_000, 3_000_000_000_000, 4_000_000_000_000, 5_000_000_000_000, long.MaxValue })
-        {
-            monkeys["humn"] = new YellingMonkey(yell);
-
-            var left = monkeys[rootMonkey.Left];
-            var right = monkeys[rootMonkey.Right];
-
-            Logger($"Yelling {yell:#,0}: {left.Evaluate(monkeys):#,0} == {right.Evaluate(monkeys):#,0}");
-        }
-
-        Logger("==================");
-
-        // Right number is always the same
-        // So, we can use "insertion sort" to find the correct left number.
-        // Basically, start with long Max Value
-        // Eval 
-
         var source = monkeys[rootMonkey.Left];
         var target = monkeys[rootMonkey.Right].Evaluate(monkeys);
 
-        var reverse = TryYell(target, source, 100, monkeys) > target;
-
-        Logger("reverse: " + reverse);
-        Logger("==================");
-
-        //var lower = 0L;
-        ////var upper = monkeys.Count == 15 ? 5000 : long.MaxValue;
-        //var upper = 10000L; //long.MaxValue;
+        var reverse = TryYell(source, 100, monkeys) > target;
 
         var lower = reverse ? 2_000_000_000_000 : 0L;
-        var upper = reverse ? 5_000_000_000_000 : long.MaxValue; //long.MaxValue;
-
-        //const string fmt = "#,0";
+        var upper = reverse ? 5_000_000_000_000 : long.MaxValue;
 
         while (lower <= upper)
         {
             var candidateYell = (lower + upper) / 2;
 
-            var result = TryYell(target, source, candidateYell, monkeys);
+            var result = TryYell(source, candidateYell, monkeys);
 
             if (result == target)
             {
@@ -68,47 +39,23 @@ public class Day21Solver : ISolver
             }
 
             var isLower = reverse ? !(result < target) : result < target;
-            //if (reverse)
-            //{
-            //    isLower
-            //}
 
             if (isLower)
             {
-                lower = candidateYell + 1; //result + 1;
-
-                //upper = candidateYell - 1; //result - 1;
-                // example: lower = Math.Max(candidateYell + 1, 0); //result + 1;
+                lower = candidateYell + 1;
             }
             else
             {
-                upper = candidateYell - 1; //result - 1;
-
-                //lower = candidateYell + 1; //result + 1;
-                // example: upper = candidateYell - 1; //result - 1;
+                upper = candidateYell - 1;
             }
-
-            //Logger($"{new { candidateYell = candidateYell.ToString("N"), result, lower, upper, target, isLower = result < target }}");
 
             Logger($"yell: {candidateYell:#,0}, result: {result:#,0}, lower: {lower:#,0}, upper: {upper:#,0}, target: {target:#,0}, isLower: {result < target}");
         }
 
         throw new InvalidOperationException("No number found to pass root's equality test");
-
-        //foreach (var yell in new[] { 0, 1, 301, 1000, 10000 })
-        //{
-        //    monkeys["humn"] = new YellingMonkey(yell);
-
-        //    var left = monkeys[rootMonkey.Left];
-        //    var right = monkeys[rootMonkey.Right];
-
-        //    Logger($"Yelling {yell}: {left.Evaluate(monkeys)} == {right.Evaluate(monkeys)}");
-        //}
-
-        //return null;
     }
 
-    private static long TryYell(long target, Monkey source, long yell, Dictionary<string, Monkey> monkeys)
+    private static long TryYell(Monkey source, long yell, Dictionary<string, Monkey> monkeys)
     {
         monkeys["humn"] = new YellingMonkey(yell);
         return source.Evaluate(monkeys);
@@ -129,32 +76,32 @@ public class Day21Solver : ISolver
         return monkey;
     }).ToDictionary(monkey => monkey.Id);
 
-    public abstract class Monkey
+    abstract class Monkey
     {
         public string Id { get; set; } = "";
 
         public abstract long Evaluate(IReadOnlyDictionary<string, Monkey> monkeys);
     }
 
-    public sealed class YellingMonkey : Monkey
+    sealed class YellingMonkey : Monkey
     {
-        public YellingMonkey(long value) => Value = value;
+        readonly long _value;
 
-        public long Value { get; }
+        public YellingMonkey(long value) => _value = value;
 
-        public override long Evaluate(IReadOnlyDictionary<string, Monkey> monkeys) => Value;
+        public override long Evaluate(IReadOnlyDictionary<string, Monkey> monkeys) => _value;
     }
 
-    public sealed class MathMonkey : Monkey
+    sealed class MathMonkey : Monkey
     {
+        readonly char _operator;
+
         public MathMonkey(char @operator, string left, string right)
         {
-            Operator = @operator;
+            _operator = @operator;
             Left = left;
             Right = right;
         }
-
-        public char Operator { get; }
 
         public string Left { get; }
 
@@ -165,13 +112,13 @@ public class Day21Solver : ISolver
             var left = monkeys[Left];
             var right = monkeys[Right];
 
-            return Operator switch
+            return _operator switch
             {
                 '+' => left.Evaluate(monkeys) + right.Evaluate(monkeys),
                 '-' => left.Evaluate(monkeys) - right.Evaluate(monkeys),
                 '*' => left.Evaluate(monkeys) * right.Evaluate(monkeys),
                 '/' => left.Evaluate(monkeys) / right.Evaluate(monkeys),
-                _ => throw new InvalidOperationException("Invalid operation: " + Operator)
+                _ => throw new InvalidOperationException("Invalid operation: " + _operator)
             };
         }
     }
