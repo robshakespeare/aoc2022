a91323de564ff8259e37aa9354aaefa4484e4057
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2022-12-21 05:33:23 +0000

    resume here!!!

--- AoC/Day20/Day20Solver.cs
+++ AoC/Day20/Day20Solver.cs
@@ -1,6 +1,3 @@
-using System.Collections.Generic;
-using System.Globalization;
-
 namespace AoC.Day20;
 
 public class Day20Solver : ISolver
@@ -11,14 +8,14 @@ public class Day20Solver : ISolver
 
     public long? SolvePart2(PuzzleInput input) => SumGroveCoordinates(Decrypt(input, numOfCycles: 10, decryptionKey: 811589153));
 
-    public class Number
-    {
-        public long Value { get; }
+    //public class Number
+    //{
+    //    public long Value { get; }
 
-        public Number(long value) => Value = value;
+    //    public Number(long value) => Value = value;
 
-        public override string ToString() => Value.ToString();
-    }
+    //    public override string ToString() => Value.ToString();
+    //}
 
     public static IList<long> Decrypt(string input, int numOfCycles = 1, int decryptionKey = 1)
     {
@@ -90,13 +87,19 @@ public class Day20Solver : ISolver
         var enableSkip = false;
 
         var list = node.List ?? throw new InvalidOperationException("No list");
-        var cycleSize = list.Count - 1L;
+        //var cycleSize = list.Count; // - 1L;
 
         var movement = Math.Abs(node.Value);
         var moveForwards = node.Value > 0;
 
+        Console.WriteLine("Move number start");
+
         if (moveForwards)
         {
+            var edge = false;
+            var skip = -1L;
+            var i = 0;
+
             //for (long counter = 0; counter < movement; counter++)
             for (var counter = movement; counter > 0; counter--)
             {
@@ -105,6 +108,11 @@ public class Day20Solver : ISolver
                     // Move to start plus 1. i.e. one after start
                     list.Remove(node);
                     list.AddAfter(list.First ?? throw new InvalidOperationException("No first"), node);
+
+                    //counter %= cycleSize;
+
+                    edge = false;
+                    skip = -1;
                 }
                 else if (node.Next == list.Last /*|| node.Next == null*/) // i.e. we're the second to last in the list
                 {
@@ -113,15 +121,24 @@ public class Day20Solver : ISolver
                     list.AddFirst(node);
 
                     ////#######
-                    if (enableSkip)
-                    {
-                        counter %= cycleSize;
-                        if (counter == 0)
-                        {
-                            counter = cycleSize;
-                        }
-                    }
+                    //if (enableSkip)
+                    //{
+                    //    counter %= cycleSize;
+                    //    if (counter == 0)
+                    //    {
+                    //        counter = cycleSize;
+                    //    }
+                    //}
                     ////#######
+
+                    //counter %= cycleSize;
+
+                    edge = true;
+                    skip = counter % (list.Count - 1); //Math.Clamp(counter % cycleSize, 1, cycleSize); //Math.Max(counter % cycleSize, 1);
+                    if (skip == 0)
+                    {
+                        skip = list.Count - 1;
+                    }
                 }
                 else
                 {
@@ -129,14 +146,25 @@ public class Day20Solver : ISolver
                                  throw new InvalidOperationException("Unexpected: should cycle, not reach end");
                     list.Remove(node);
                     list.AddAfter(source, node);
+
+                    edge = false;
+                    skip = -1;
                 }
+
+                if ((movement - counter) < 20 || i <= 20)
+                {
+                    Console.WriteLine($"{counter,4}: {i,4} - {string.Join(", ", list)} -- {edge} {(edge ? skip : null)}");
+
+                }
+                i++;
             }
         }
         else
         {
             //var reachedEdge = false;
-            //var edge = false;
-            //var skip = -1L;
+            var edge = false;
+            var skip = -1L;
+            var i = 0;
 
             //for (long counter = 0; counter < movement; counter++)
             for (var counter = movement; counter > 0; counter--)
@@ -147,6 +175,17 @@ public class Day20Solver : ISolver
                     list.Remove(node);
                     list.AddBefore(list.Last ?? throw new InvalidOperationException("No last"), node);
 
+                    //counter %= cycleSize;
+
+                    //edge = true;
+                    //skip = counter % (list.Count - 1); //Math.Clamp(counter % cycleSize, 1, cycleSize); //Math.Max(counter % cycleSize, 1);
+                    //if (skip == 0)
+                    //{
+                    //    skip = list.Count - 1;
+                    //}
+
+                    edge = false;
+                    skip = -1;
                 }
                 else if (node.Previous == list.First /*|| node.Previous == null*/) // i.e. we're the second in the list
                 {
@@ -168,22 +207,24 @@ public class Day20Solver : ISolver
                     //    counter = GetIt2(list, counter);
                     //}
 
-                    //edge = true;
-                    //skip = counter % cycleSize; //Math.Clamp(counter % cycleSize, 1, cycleSize); //Math.Max(counter % cycleSize, 1);
-                    //if (skip == 0)
-                    //{
-                    //    skip = cycleSize;
-                    //}
-
-                    ////#######
-                    if (enableSkip)
+                    edge = true;
+                    skip = counter % (list.Count - 1); //Math.Clamp(counter % cycleSize, 1, cycleSize); //Math.Max(counter % cycleSize, 1);
+                    if (skip == 0)
                     {
-                        counter %= cycleSize;
-                        if (counter == 0)
-                        {
-                            counter = cycleSize;
-                        }
+                        skip = list.Count - 1;
                     }
+
+                    //counter %= cycleSize;
+
+                    //////#######
+                    //if (enableSkip)
+                    //{
+                    //    counter %= cycleSize;
+                    //    if (counter == 0)
+                    //    {
+                    //        counter = cycleSize;
+                    //    }
+                    //}
                     ////#######
                 }
                 else
@@ -193,10 +234,18 @@ public class Day20Solver : ISolver
                     list.Remove(node);
                     list.AddBefore(source, node);
 
-                    //edge = false;
-                    //skip = -1;
+                    edge = false;
+                    skip = -1;
                 }
 
+                if ((movement - counter) < 20 || i <= 20)
+                {
+                    Console.WriteLine($"{counter,4}: {i,4} - {string.Join(", ", list)} -- {edge} {(edge ? skip : null)}");
+
+                }
+                //Console.WriteLine($"{counter,4}: {i,4} {string.Join(", ", list)} -- {edge} {(edge ? skip : null)}");
+
+                i++;
                 //if (reachedEdge || true)
                 //{
                 //    Console.WriteLine($"{counter,4}: {string.Join(", ", list)} -- {edge} {(edge ? skip : null)}");
@@ -205,62 +254,62 @@ public class Day20Solver : ISolver
         }
     }
 
-    public static void MoveNumber(Number number, List<Number> numbers)
-    {
-        // Move the node the number of times indicated by its value!
-        // Note that we can forwards or backwards!
-        var numOfMoves = Math.Abs(number.Value);
-        //var moveForwards = number.Value > 0;
-        var dir = number.Value > 0 ? 1 : -1;
+    //public static void MoveNumber(Number number, List<Number> numbers)
+    //{
+    //    // Move the node the number of times indicated by its value!
+    //    // Note that we can forwards or backwards!
+    //    var numOfMoves = Math.Abs(number.Value);
+    //    //var moveForwards = number.Value > 0;
+    //    var dir = number.Value > 0 ? 1 : -1;
 
-        // Move the number, until we have completed all of our moves
-        for (var i = 0L; i < numOfMoves; i++)
-        {
-            // Get the current index of the number
-            // rs-todo: ugg, there must be a moe optimal way of doing this!!
-            var currentIndex = numbers.IndexOf(number);
-            if (currentIndex == -1)
-            {
-                throw new InvalidOperationException("Unexpected: did not find number in list: " + number);
-            }
+    //    // Move the number, until we have completed all of our moves
+    //    for (var i = 0L; i < numOfMoves; i++)
+    //    {
+    //        // Get the current index of the number
+    //        // rs-todo: ugg, there must be a moe optimal way of doing this!!
+    //        var currentIndex = numbers.IndexOf(number);
+    //        if (currentIndex == -1)
+    //        {
+    //            throw new InvalidOperationException("Unexpected: did not find number in list: " + number);
+    //        }
 
-            // Loop around if needed
-            if (dir == -1 && currentIndex == 0)
-            {
-                // Move to end less one. i.e. one before end
-                numbers.RemoveAt(currentIndex);
-                numbers.Insert(numbers.Count - 1, number);
-            }
-            else if (dir == -1 && currentIndex == 1)
-            {
-                // Move to end.
-                numbers.RemoveAt(currentIndex);
-                numbers.Insert(numbers.Count, number);
-            }
-            else if (dir == 1 && currentIndex == numbers.Count - 1)
-            {
-                // Move to start plus 1. i.e. one after start
-                numbers.RemoveAt(currentIndex);
-                numbers.Insert(1, number);
-            }
-            else if (dir == 1 && currentIndex == numbers.Count - 2)
-            {
-                // Move to start
-                numbers.RemoveAt(currentIndex);
-                numbers.Insert(0, number);
-            }
-            else
-            {
-                var prevIndex = currentIndex;
-                currentIndex += dir;
+    //        // Loop around if needed
+    //        if (dir == -1 && currentIndex == 0)
+    //        {
+    //            // Move to end less one. i.e. one before end
+    //            numbers.RemoveAt(currentIndex);
+    //            numbers.Insert(numbers.Count - 1, number);
+    //        }
+    //        else if (dir == -1 && currentIndex == 1)
+    //        {
+    //            // Move to end.
+    //            numbers.RemoveAt(currentIndex);
+    //            numbers.Insert(numbers.Count, number);
+    //        }
+    //        else if (dir == 1 && currentIndex == numbers.Count - 1)
+    //        {
+    //            // Move to start plus 1. i.e. one after start
+    //            numbers.RemoveAt(currentIndex);
+    //            numbers.Insert(1, number);
+    //        }
+    //        else if (dir == 1 && currentIndex == numbers.Count - 2)
+    //        {
+    //            // Move to start
+    //            numbers.RemoveAt(currentIndex);
+    //            numbers.Insert(0, number);
+    //        }
+    //        else
+    //        {
+    //            var prevIndex = currentIndex;
+    //            currentIndex += dir;
 
-                // Otherwise, swap
-                (numbers[currentIndex], numbers[prevIndex]) = (numbers[prevIndex], numbers[currentIndex]);
-            }
+    //            // Otherwise, swap
+    //            (numbers[currentIndex], numbers[prevIndex]) = (numbers[prevIndex], numbers[currentIndex]);
+    //        }
 
-            // rs-todo: I think we can apply the "skip" logic here
-        }
-    }
+    //        // rs-todo: I think we can apply the "skip" logic here
+    //    }
+    //}
 
     static long SumGroveCoordinates(IList<long> decrypted)
     {
