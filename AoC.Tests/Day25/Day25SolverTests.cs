63a3d42a46ec93789f93ee3b512f62aaf76a1e3b
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2022-12-25 10:38:08 +0000

    Day 25 done, that was harder than it should have been :(

--- AoC.Tests/Day25/Day25SolverTests.cs
+++ AoC.Tests/Day25/Day25SolverTests.cs
@@ -6,6 +6,8 @@ public class Day25SolverTests
 {
     private readonly Day25Solver _sut = new();
 
+    static Day25SolverTests() => Day25Solver.Logger = TestContext.Progress.WriteLine;
+
     private const string ExampleInput = """
         1=-0-2
         12111
@@ -23,6 +25,36 @@ public class Day25SolverTests
         """;
 
     public record NormalNumberSnafuCounterpart(int Number, string Snafu);
+
+    //public static IReadOnlyList<TestCaseData> NormalNumberSnafuCounterparts { get; } = """
+    //           -3             -2
+    //           -2              =
+    //           -1              -
+    //            0              0
+    //            1              1
+    //            2              2
+    //            3             1=
+    //            4             1-
+    //            5             10
+    //            6             11
+    //            7             12
+    //            8             2=
+    //            9             2-
+    //           10             20
+    //           15            1=0
+    //           20            1-0
+    //           63            222
+    //          125           1000
+    //          625          10000
+    //          976          2=-01
+    //         2022         1=11-2
+    //        12345        1-0---0
+    //    314159265  1121-1110-1=0
+    //    """.ReadLines().Select(line =>
+    //{
+    //    var parts = line.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    //    return new TestCaseData(new NormalNumberSnafuCounterpart(int.Parse(parts[0]), parts[1]));
+    //}).ToArray();
 
     public static IReadOnlyList<TestCaseData> NormalNumberSnafuCounterparts { get; } = """
                 0              0
@@ -51,10 +83,15 @@ public class Day25SolverTests
                23            10=
                24            10-
                25            100
+               61            221
+               62            222
+               63           1===
               122           10-2
               123           100=
               124           100-
               125           1000
+              312           2222
+              313          1====
               625          10000
               976          2=-01
              2022         1=11-2
@@ -125,7 +162,7 @@ public class Day25SolverTests
         var part1Result = _sut.SolvePart1();
 
         // ASSERT
-        part1Result.Should().Be(null);
+        part1Result.Should().Be("20=212=1-12=200=00-1");
     }
 
     [Test]
--- AoC/Day25/Day25Solver.cs
+++ AoC/Day25/Day25Solver.cs
@@ -11,6 +11,7 @@
 // .                                                                                                     .
 // .:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:._.:*~*:*~*:._.:*~*:.
 
+using System.Numerics;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace AoC.Day25;
@@ -22,13 +23,13 @@ public class Day25Solver : ISolver<string, string>
     /// <summary>
     /// What SNAFU number do you supply to Bob's console?
     /// </summary>
-    public string? SolvePart1(PuzzleInput input)
+    public string SolvePart1(PuzzleInput input)
     {
         var sumOfFuelRequirements = input.ReadLines().Select(SnafuToNormalNumber).Sum();
 
         Console.WriteLine($"Sum of the fuel requirements: {sumOfFuelRequirements}");
 
-        return null;
+        return NormalNumberToSnafu(sumOfFuelRequirements);
     }
 
     public string? SolvePart2(PuzzleInput input)
@@ -55,7 +56,7 @@ public class Day25Solver : ISolver<string, string>
     {
         var n = 1;
 
-        while (Math.Pow(5, n) / 2 <= number)
+        while (Math.Pow(5, n) / 2 <= number) // NOTE: we need the extra digit once number becomes greater than half of what can be done in that range
         {
             n++;
         }
@@ -63,10 +64,160 @@ public class Day25Solver : ISolver<string, string>
         return n;
     }
 
-    private static char[] baseChars = { '2', '1', '0', '-', '=' };
+    private static char[] baseChars = { '0', '1', '2', '=', '-' };
 
-    public static string NormalNumberToSnafu(long value)
+    private static (char snafuDigit, int delta)[] BaseChars =
     {
+        ('2', 2),
+        ('1', 1),
+        ('0', 0),
+        ('-', -1),
+        ('=', -2)
+    };
+
+    public static Action<string> Logger = Console.WriteLine;
+
+    public static string NormalNumberToSnafu(long number)
+    {
+        var result = new StringBuilder();
+        var targetBase = baseChars.Length;
+
+        do
+        {
+            var prev = number;
+            var rem = number % targetBase;
+
+            Logger(rem.ToString());
+
+            var chr = baseChars[rem];
+
+            //result = baseChars[rem] + result;
+            result.Insert(0, chr);// + result;
+
+            if (chr is '-' or '=')
+            {
+                number += targetBase;
+            }
+
+            number /= targetBase;
+
+            Console.WriteLine(new { prev, number, rem, result });
+        }
+        while (number > 0);
+
+        return result.ToString();
+
+        var snafuDigitsRequired = SnafuDigitsRequired(number);
+
+        string snafuDigits = ""; // rs-todo: use string builder
+
+        var remain = (long)Math.Pow(5, snafuDigitsRequired) / 2;
+
+        //Console.WriteLine(remain);
+
+        for (var n = snafuDigitsRequired - 1; n >= 0; n--)
+        {
+            var b = (long)Math.Pow(5, n); // NOTE: DEFINITELY CORRECT, MATCH (b * u) above.  Jst need to work out u!!!!
+            //Console.WriteLine(new { number, n, b, u1 = b * 2, u2 = b * 1, u3 = b * 0, u4 = b * -1, u5 = b * -2 });
+
+            var test = BaseChars.Select(baseChar => number + b * baseChar.delta).First(nextNum => nextNum >= 0);
+
+            // rs-todo: choose the first u that doesn't take next number below zero
+
+            Console.WriteLine(new { number, n, b, test, u1 = b * 2, u2 = b * 1, u3 = b * 0, u4 = b * -1, u5 = b * -2 });
+
+
+            continue;
+
+            
+
+            //var range = (long)Math.Pow(5, n + 1);
+
+            var actRange = (long)Math.Pow(5, n + 1);
+
+            var range = (long)Math.Pow(5, n + 1);
+            var max = range / 2;
+            var min = ((long)Math.Pow(5, n) / 2) + 1; // rs-todo: min is correct for everything but first "bracket", so IF NEED min, then MUST FIX
+
+            //if (number / (decimal)range > 0.3m)
+            //{
+            //    // we need whole of it??
+            //    snafuDigits += '2';
+            //}
+            //else if (number / (decimal)range > 0.1m)
+            //{
+            //    // we need whole of it??
+            //    snafuDigits += '1';
+            //}
+
+            // NOTE: number will never be greater than max
+
+
+            // normal range = (5 ^ n) - 1
+            // so, our range = (5 ^ n) - 1 - ()
+
+            //var range = (long)Math.Pow(5, n + 1);
+
+            //var snafuRequired = number > (range / 2); // <= number;
+
+            //Console.WriteLine(new { number, n, b, actRange }); //, range, snafuRequired, keh = (range / 2) });
+
+            //Console.WriteLine(new { number, n, b, range, actRange, min, max, snafuDigits, hmm = number / (decimal)range } ); //, range, snafuRequired, keh = (range / 2) });
+
+            var hmm2 = number / (decimal)b;
+
+            if (hmm2 <= 1)
+            {
+                snafuDigits += '2';
+            }
+            else if (hmm2 <= 2)
+            {
+                snafuDigits += '2';
+            }
+            else
+            {
+                //throw new InvalidOperationException("not yet supported!");
+            }
+
+            Console.WriteLine(new { snafuDigits, number, n, b, range, min, max, hmm2, hmm = number / (decimal)range }); //, range, snafuRequired, keh = (range / 2) });
+
+            //break;
+
+            //var backBySize = b / 5;
+            //var maxBackBySize = backBySize * 2;
+
+            ////var (res, rem) = Math.DivRem(value, b);
+
+            //var res = value / (float)b;
+            //var resR = Math.Round(value / (float)b);
+            //var need = res - Math.Floor(res) > 0.5;
+            ////var rem = value % b;
+
+            //if (value > b)
+            //{
+            //    snafuDigits += resR;
+            //}
+
+            //Console.WriteLine(new { value, n, b, res, resR, need, snafuDigits /*, backBySize, maxBackBySize, snafuDigits*//*, res, rem*/ });
+
+
+
+            //break;
+
+            //if (value >= maxBackBySize)
+            //{
+            //    snafuDigits += '=';
+            //    //value -= maxBackBySize;
+            //}
+            //else if (value >= backBySize)
+            //{
+            //    snafuDigits += '-';
+            //    //value -= maxBackBySize;
+            //}
+
+            //Console.WriteLine(new { value, b, backBySize, maxBackBySize, snafuDigits });
+        }
+
         // we can only go back -2
 
         return "rs-todo!";
